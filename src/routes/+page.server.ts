7ba94987e182352a13c3aebccac05dc251800ce8
PAD <79337368+ala-garbaa-pro@users.noreply.github.com>
2024-01-21 16:05:36 +0100

    up try 2

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,11 @@
-import { db } from '$lib/fakeDB';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals: { pba } }) => {
     console.log(`🟩 /+page.server.ts -> load`);
 
-    const nfts = db;
-    // const nfts = await pba.collection('nfts').getFullList({
-    //     sort: '-created'
-    // });
+    const nfts = await pba.collection('nfts').getFullList({
+        sort: '-created'
+    });
 
     return { nfts };
 };
